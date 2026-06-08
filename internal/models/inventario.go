f945798bb4d4bccac2ebe479385490acd640a541
Yukii034 <pachirisu04@gmail.com>
2026-06-08 17:07:56 -0500

    refactor: Arreglo de nombres en structs (Publicacion e Inventario)

--- internal/models/inventario.go
+++ internal/models/inventario.go
@@ -7,11 +7,11 @@ type Inventario struct {
 	Nombre         string    `json:"nombre" gorm:"not null"`
 	Descripcion    string    `json:"descripcion"`
 	Categoria      string    `json:"categoria"`
-	Estado_Objeto  string    `json:"estado_objeto"`
+	EstadoObjeto   string    `json:"estado_objeto"`
 	Disponibilidad string    `json:"disponibilidad"`
 	Cantidad       int       `json:"cantidad"`
 	Foto_Url       string    `json:"foto_url"`
-	Created_at     time.Time `json:"created_at"`
-	Updated_at     time.Time `json:"updated_at"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 	UsuarioID      int       `json:"usuario_id"`
 }
--- internal/models/publicacion.go
+++ internal/models/publicacion.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Publicacion struct {
-	ID                 int       `json:"id" gorm:"primaryKey"`
-	Titulo             string    `json:"titulo" gorm:"not null"`
-	Tipo_Oferta        string    `json:"tipo_oferta"`
-	Estado_Publicacion int       `json:"stock"`
-	Mensaje            string    `json:"mensaje"`
-	Created_at         time.Time `json:"created_at"`
-	Updated_at         time.Time `json:"updated_at"`
-	UsuarioID          int       `json:"usuario_id"`
-	InventarioID       int       `json:"inventario_id"`
+	ID                int       `json:"id" gorm:"primaryKey"`
+	Titulo            string    `json:"titulo" gorm:"not null"`
+	TipoOferta        string    `json:"tipo_oferta"`
+	EstadoPublicacion string    `json:"estado_publicacion"`
+	Mensaje           string    `json:"mensaje"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+	UsuarioID         int       `json:"usuario_id"`
+	InventarioID      int       `json:"inventario_id"`
 }
