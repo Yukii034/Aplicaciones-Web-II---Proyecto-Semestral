228893caffa56d348896c895dd6025e3b116d45d
Bladerm <josemanuelcastilloloor@gmail.com>
2026-06-09 20:56:32 -0500

    feat: se colocaron las rutas de: Reputacion, Logro_Usuario, Logro, Calificacion en el main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,7 +20,14 @@ func main() {
 	if err != nil {
 		log.Fatal("no se pudo abrir la base de datos: ", err)
 	}
-	if err := db.AutoMigrate(&models.Inventario{}, &models.Publicacion{}); err != nil {
+	if err := db.AutoMigrate(
+		&models.Inventario{},
+		&models.Publicacion{},
+		&models.Reputacion{},
+		&models.Logro_Usuario{},
+		&models.Logro{},
+		&models.Calificacion{},
+	); err != nil {
 		log.Fatal("falló AutoMigrate: ", err)
 	}
 
@@ -49,6 +56,30 @@ func main() {
 		r.Get("/publicaciones/{id}", servidor.ObtenerPublicacion)
 		r.Put("/publicaciones/{id}", servidor.ActualizarPublicacion)
 		r.Delete("/publicaciones/{id}", servidor.BorrarPublicacion)
+
+		r.Get("/reputaciones", servidor.ListarReputacion)
+		r.Post("/reputaciones", servidor.CrearReputacion)
+		r.Get("/reputaciones/{id}", servidor.ObtenerReputacion)
+		r.Put("/reputaciones/{id}", servidor.ActualizarReputacion)
+		r.Delete("/reputaciones/{id}", servidor.BorrarReputacion)
+
+		r.Get("/logro_usuarios", servidor.ListarLogro_Usuario)
+		r.Post("/logro_usuarios", servidor.CrearLogro_Usuario)
+		r.Get("/logro_usuarios/{id}", servidor.ObtenerLogro_Usuario)
+		r.Put("/logro_usuarios/{id}", servidor.ActualizarLogro_Usuario)
+		r.Delete("/logro_usuarios/{id}", servidor.BorrarLogro_Usuario)
+
+		r.Get("/logros", servidor.ListarLogro)
+		r.Post("/logros", servidor.CrearLogro)
+		r.Get("/logros/{id}", servidor.ObtenerLogro)
+		r.Put("/logros/{id}", servidor.ActualizarLogro)
+		r.Delete("/logros/{id}", servidor.BorrarLogro)
+
+		r.Get("/calificaciones", servidor.ListarCalificacion)
+		r.Post("/calificaciones", servidor.CrearCalificacion)
+		r.Get("/calificaciones/{id}", servidor.ObtenerCalificacion)
+		r.Put("/calificaciones/{id}", servidor.ActualizarCalificacion)
+		r.Delete("/calificaciones/{id}", servidor.BorrarCalificacion)
 	})
 
 	log.Println("Servidor escuchando en http://localhost:8080")
