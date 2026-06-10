0c9d862c0e576503ae35586d17f9a0f3891efc1e
Bladerm <josemanuelcastilloloor@gmail.com>
2026-06-09 19:28:03 -0500

    feat: se agrego al archivo de almacen las interfaces de: reputacion, logro:_usuario, logro y calificaciones.
    feat: en almacen_sqlite se agrego en la funcion "SembrarSiVacio" lo que es: reputacion, logro:_usuario, logro y calificaciones.

--- internal/storage/almacen.go
+++ internal/storage/almacen.go
@@ -16,4 +16,32 @@ type Almacen interface {
 	CrearPublicacion(p models.Publicacion) models.Publicacion
 	ActualizarPublicacion(id int, datos models.Publicacion) (models.Publicacion, bool)
 	BorrarPublicacion(id int) bool
+
+	// Reputacion
+	ListarReputacion() []models.Reputacion
+	BuscarReputacionPorID(id int) (models.Reputacion, bool)
+	CrearReputacion(r models.Reputacion) models.Reputacion
+	ActualizarReputacion(id int, datos models.Reputacion) (models.Reputacion, bool)
+	BorrarReputacion(id int) bool
+
+	// Logro_Usuario
+	ListarLogro_Usuario() []models.Logro_Usuario
+	BuscarLogro_UsuarioPorID(id int) (models.Logro_Usuario, bool)
+	CrearLogro_Usuario(lu models.Logro_Usuario) models.Logro_Usuario
+	ActualizarLogro_Usuario(id int, datos models.Logro_Usuario) (models.Logro_Usuario, bool)
+	BorrarLogro_Usuario(id int) bool
+
+	//Logro
+	ListarLogro() []models.Logro
+	BuscarLogroPorID(id int) (models.Logro, bool)
+	CrearLogro(l models.Logro) models.Logro
+	ActualizarLogro(id int, datos models.Logro) (models.Logro, bool)
+	BorrarLogro(id int) bool
+
+	//Calificacion
+	ListarCalificacion() []models.Calificacion
+	BuscarCalificacionPorID(id int) (models.Calificacion, bool)
+	CrearCalificacion(ca models.Calificacion) models.Calificacion
+	ActualizarCalificacion(id int, datos models.Calificacion) (models.Calificacion, bool)
+	BorrarCalificacion(id int) bool
 }
--- internal/storage/almacen_sqlite.go
+++ internal/storage/almacen_sqlite.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"proyecto-semestral/internal/models"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -33,6 +34,34 @@ func (a *AlmacenSQLite) SembrarSiVacio() {
 		{Titulo: "Dono microondas", TipoOferta: "donacion", EstadoPublicacion: "disponible", Mensaje: "Pasar a retirar", UsuarioID: 1, InventarioID: 2},
 	}
 	a.db.Create(&publicaciones)
+
+	reputaciones := []models.Reputacion{
+		{PuntosTotales: 150, Nivel: 2, AcuerdosCompl: 5, CalificacionPromedio: 4.5, UsuarioID: 1},
+		{PuntosTotales: 80, Nivel: 1, AcuerdosCompl: 2, CalificacionPromedio: 3, UsuarioID: 2},
+		{PuntosTotales: 320, Nivel: 4, AcuerdosCompl: 12, CalificacionPromedio: 4.9, UsuarioID: 3},
+	}
+	a.db.Create(&reputaciones)
+
+	logro_usuario := []models.Logro_Usuario{
+		{FechaDesbl: time.Now(), UsuarioID: 1, LogroID: 1},
+		{FechaDesbl: time.Now(), UsuarioID: 2, LogroID: 2},
+		{FechaDesbl: time.Now(), UsuarioID: 3, LogroID: 1},
+	}
+	a.db.Create(&logro_usuario)
+
+	logro := []models.Logro{
+		{Nombre: "Primer intercambio", Descripcion: "Completa tu primer trueque", PuntosRequeridos: 50},
+		{Nombre: "Donador solidario", Descripcion: "Realizaste tu primera donacion", PuntosRequeridos: 30},
+		{Nombre: "Usuario confiable", Descripcion: "Alcanzaste una Calificacion mayor a 4.5", PuntosRequeridos: 100},
+	}
+	a.db.Create(&logro)
+
+	calificaciones := []models.Calificacion{
+		{Comentarios: "Muy buen intercambio, puntual y amable", UsuarioID: 1, AcuerdoID: 1},
+		{Comentarios: "Todo perfecto, recomendado", UsuarioID: 2, AcuerdoID: 2},
+		{Comentarios: "Buena experiencia en general", UsuarioID: 3, AcuerdoID: 1},
+	}
+	a.db.Create(&calificaciones)
 }
 
 var _ Almacen = (*AlmacenSQLite)(nil)
