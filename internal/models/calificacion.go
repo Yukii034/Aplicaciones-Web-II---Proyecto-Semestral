7a30fd007be70b440758df0ff6f65c9a7c1d34c6
Bladerm <josemanuelcastilloloor@gmail.com>
2026-06-10 20:11:07 -0500

    feat actualizacion del json de comentarios

--- internal/models/calificacion.go
+++ internal/models/calificacion.go
@@ -4,7 +4,7 @@ import "time"
 
 type Calificacion struct {
 	ID          int       `json:"id" gorm:"primaryKey"`
-	Comentarios string    `json:"puntos_totales" gorm:"not null"`
+	Comentarios string    `json:"comentarios" gorm:"not null"`
 	UsuarioID   int       `json:"usuario_id"`
 	AcuerdoID   int       `json:"acuerdo_id"`
 	CreatedAt   time.Time `json:"created_at"`
