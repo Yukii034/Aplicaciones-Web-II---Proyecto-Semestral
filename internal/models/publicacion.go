e4d7985d715258d561e646119514349ffc9c1e0f
Yukii034 <pachirisu04@gmail.com>
2026-07-06 18:51:17 -0500

    feat: añadida la relacion formal con gorm en publicacion y coleccion de postman de publicacion e inventario

--- internal/models/publicacion.go
+++ internal/models/publicacion.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type Publicacion struct {
-	ID                int       `json:"id" gorm:"primaryKey"`
-	Titulo            string    `json:"titulo" gorm:"not null"`
-	TipoOferta        string    `json:"tipo_oferta"`
-	EstadoPublicacion string    `json:"estado_publicacion"`
-	Mensaje           string    `json:"mensaje"`
-	CreatedAt         time.Time `json:"created_at"`
-	UpdatedAt         time.Time `json:"updated_at"`
-	UsuarioID         int       `json:"usuario_id"`
-	InventarioID      int       `json:"inventario_id"`
+	ID                int        `json:"id" gorm:"primaryKey"`
+	Titulo            string     `json:"titulo" gorm:"not null"`
+	TipoOferta        string     `json:"tipo_oferta"`
+	EstadoPublicacion string     `json:"estado_publicacion"`
+	Mensaje           string     `json:"mensaje"`
+	CreatedAt         time.Time  `json:"created_at"`
+	UpdatedAt         time.Time  `json:"updated_at"`
+	UsuarioID         int        `json:"usuario_id"`
+	InventarioID      int        `json:"inventario_id"`
+	Inventario        Inventario `json:"inventario,omitempty" gorm:"foreignKey:InventarioID"` // relacion de gorm formal
 }
